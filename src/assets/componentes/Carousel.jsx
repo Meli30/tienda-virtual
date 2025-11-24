e41bb75e771de4aff69cc9a0c18d426d490fa2f5
melisa <melif6108@gmail.com>
2025-11-24 14:59:57 -0300

    carousel responsive

--- src/assets/componentes/Carousel.jsx
+++ src/assets/componentes/Carousel.jsx
@@ -18,42 +18,66 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="relative w-full mx-auto overflow-hidden rounded-2xl shadow-lg">
-      <img
-        src={images[current]}
-        alt={`Imagen ${current + 1}`}
-        className="w-full h-150
-         object-cover transition-all duration-700 ease-in-out"
+    <div className="relative w-full max-w-full mx-auto overflow-hidden rounded-none sm:rounded-2xl shadow-lg">
+
+  {/* Imagen responsive */}
+  <img
+    src={images[current]}
+    alt={`Imagen ${current + 1}`}
+    className="
+      w-full 
+      h-[220px]       /* altura en celular */
+      sm:h-[350px]    /* altura en tablet */
+      md:h-[450px]    /* altura en desktop */
+      object-cover 
+      transition-all duration-700 ease-in-out
+    "
+  />
+
+  {/* Botón Izquierdo */}
+  <button
+    onClick={() =>
+      setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+    }
+    className="
+      absolute left-3 top-1/2 -translate-y-1/2
+      bg-black/40 text-white 
+      p-2 sm:p-3 
+      rounded-full 
+      hover:bg-black/60
+    "
+  >
+    ❮
+  </button>
+
+  {/* Botón Derecho */}
+  <button
+    onClick={nextSlide}
+    className="
+      absolute right-3 top-1/2 -translate-y-1/2
+      bg-black/40 text-white 
+      p-2 sm:p-3 
+      rounded-full 
+      hover:bg-black/60
+    "
+  >
+    ❯
+  </button>
+
+  {/* Indicadores */}
+  <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-2">
+    {images.map((_, index) => (
+      <button
+        key={index}
+        onClick={() => setCurrent(index)}
+        className={`w-3 h-3 rounded-full ${
+          index === current ? "bg-white" : "bg-gray-400"
+        }`}
       />
+    ))}
+  </div>
+</div>
 
-      <button
-        onClick={() =>
-          setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1))
-        }
-        className="absolute left-3 top-1/2 -translate-y-1/2 bg-black/40 text-white p-2 rounded-full hover:bg-black/60"
-      >
-        ❮
-      </button>
-
-      <button
-        onClick={nextSlide}
-        className="absolute right-3 top-1/2 -translate-y-1/2 bg-black/40 text-white p-2 rounded-full hover:bg-black/60"
-      >
-        ❯
-      </button>
-
-      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-2">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrent(index)}
-            className={`w-3 h-3 rounded-full ${
-              index === current ? "bg-white" : "bg-gray-400"
-            }`}
-          />
-        ))}
-      </div>
-    </div>
   );
 };
 
